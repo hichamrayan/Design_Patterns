05aabdf083c4b99f7001d699ffb2406eba7e5a17
hichamrayan <hichamm.bouzkraoui@gmail.com>
2020-11-12 20:34:11 -0700

    update decorator pattern

--- Decorator/Example.java
+++ Decorator/Example.java
@@ -2,11 +2,11 @@ class Example{
     public static void main(String[] args){
         Decaf dec=new Decaf();
         Expresso exp=new Expresso();
-        CaramelDecorator caramel=new CaramelDecorator(dec);
-        ChoclateDecorator choclate=new ChoclateDecorator(exp);
-        caramel.cost();
-        choclate.cost();
-        ChoclateDecorator choclatewithcaramel= new ChoclateDecorator(caramel);
-        choclatewithcaramel.cost();
+        CaramelDecorator carameldecf=new CaramelDecorator(dec);
+        ChoclateDecorator choclateexpresso=new ChoclateDecorator(exp);
+        carameldecf.cost();
+        choclateexpresso.cost();
+        ChoclateDecorator choclatewithcarameldecf= new ChoclateDecorator(carameldecf);
+        choclatewithcarameldecf.cost();
     }
 }
